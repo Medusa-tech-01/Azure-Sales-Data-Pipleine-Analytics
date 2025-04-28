b075044112f4dd36aea590f9e4e0931bcedaabf5
Simran Tawar <118318465+Medusa-tech-01@users.noreply.github.com>
2025-04-28 03:52:42 -0400

    Add files via upload

---
+++ SQL Scripts/SQLQuery1.sql
@@ -0,0 +1,16 @@
+USE AdventureWorksLT2017;
+
+CREATE LOGIN sam WITH PASSWORD = 'SSMSSam@23';
+create user sam for login sam
+
+SELECT 
+s.name AS SchemaName,
+t.name AS TableName 
+FROM sys.tables t 
+INNER JOIN sys.schemas s
+ON t.schema_id = s.schema_id 
+WHERE s.name = 'SalesLT'
+
+SELECT * FROM SalesLT.Address;
+
+
